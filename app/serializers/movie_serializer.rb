cdbb6679a71e98e34768d93b641fcd68778c5d21
Zach <zachlandis91@gmail.com>
2023-06-21 15:26:50 -0600

    commit tree 3052fb44206e367ab71cbbe1ff306884dab4d04a

---
+++ app/serializers/movie_serializer.rb
@@ -0,0 +1,8 @@
+class MovieSerializer < ActiveModel::Serializer
+  attributes :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director, :summary
+
+
+  def summary
+    "#{self.object.title} - #{self.object.description[0..49]}...."
+  end
+end
